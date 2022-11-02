c3f0d54e8f159399286db4e5f6b979b25bb70b52
Beth Rissman <beth.rissman@telepham.com>
2022-11-01 20:52:20 -0500

    adding prompts

--- js/app.js
+++ js/app.js
@@ -1,40 +1,44 @@
-'use strict';
 
-console.log('Hi, from the js file!');
+'use strict';
+console.log('The JS file is alive');
 
 // Prompt users for input
-let userName = prompt('What is your name?');
+    let userName = prompt('What is your name?');
+    let toLowerCaseName = userName.toLowerCase();
+    console.log('User name is: ', userName);
+    console.log('User name data type is: ', typeof(userName));
+    console.log('Name Lower Case: ', toLowerCaseName);
 
-console.log('User name is: ', userName);
+// Validate the input using some conditional logic
 
-// Validate the input usring some conditional logic
-// if(meet this condition then do hte else){
-
-// } else {
-
+// if(typeOf(userName) = "");  {
+// alert('Welcome to the All About Me page ' + userName);
+// } 
+//     else 
+//     {
+//     alert('Please enter your name.')
 // }
 
-if(toLowerCaseName === 'bob' || lowerCaseName === 'larry') {
+// if(meet this condition then do the else){
+// } else {
+// }
+// if(toLowerCaseName === 'bob' || lowerCaseName === 'larry') {
+// alert('Welcome back to the site: ' + userName);
+// } else {
+//     alert('Welcome new user to my page!');
+// }
 
-alert('Welcome back to the site: ' + userName);
-} else {
-    alert('Welcome new user to my page!');
-}
-
-// userName.toLowerCase or the userName.toUpperCase(); 
-let toLowerCaseName = userName.toLowerCaseName();
-
-console.log('Name Lower Case: ', toLowerCaseName);
-
-
+// // userName.toLowerCase or the userName.toUpperCase(); 
+// let toLowerCaseName = userName.toLowerCaseName();
+// console.log('Name Lower Case: ', toLowerCaseName);
 
 //look at some data types: boolean, string, number, undefined, null
 
-let myBooleanValueTrue = true;
-let myBooleanValueFalse = false;
-let thirdBooleanValueTrue = true;
+// let myBooleanValueTrue = true;
+// let myBooleanValueFalse = false;
+// let thirdBooleanValueTrue = true;
 
-if
+// if
 
 
 
