faafbff548b33fe9f16f1302b8eb9dd0bae78c3d
Beth Rissman <beth.rissman@telepham.com>
2022-11-01 20:06:23 -0500

    updates to app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,48 @@
 'use strict';
 
-console.log('Hi from the js file!');
+console.log('Hi, from the js file!');
+
+// Prompt users for input
+let userName = prompt('What is your name?');
+
+console.log('User name is: ', userName);
+
+// Validate the input usring some conditional logic
+// if(meet this condition then do hte else){
+
+// } else {
+
+// }
+
+if(toLowerCaseName === 'bob' || lowerCaseName === 'larry') {
+
+alert('Welcome back to the site: ' + userName);
+} else {
+    alert('Welcome new user to my page!');
+}
+
+// userName.toLowerCase or the userName.toUpperCase(); 
+let toLowerCaseName = userName.toLowerCaseName();
+
+console.log('Name Lower Case: ', toLowerCaseName);
+
+
+
+//look at some data types: boolean, string, number, undefined, null
+
+let myBooleanValueTrue = true;
+let myBooleanValueFalse = false;
+let thirdBooleanValueTrue = true;
+
+if
+
+
+
+// introduction to arrays
+
+
+// methods we are learning
+// prompt(), typeof(), toLowerCase(), toUpperCase()
+
+// technical requirements for lab
 
