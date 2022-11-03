8321348bda9c4dffa863122d574d168f20d265b3
Beth Rissman <beth.rissman@telepham.com>
2022-11-02 21:00:35 -0500

    updates to js.app

--- js/app.js
+++ js/app.js
@@ -1,103 +1,76 @@
 
 'use strict';
 console.log('The JS file is alive');
+//global variables
+let userName; //creating a space in memory to hold some information later.
+let petInfo;
+let nativeInfo; //declaring variables
+let kidInfo;
+let travelInfo;
 
-//prompt 1
 
-// Prompt users for input
-
-    let userName = prompt('What is your name?');
-    let toLowerCaseName = userName.toLowerCase();
-    console.log('User name is: ', userName);
-    console.log('User name data type is: ', typeof(userName));
-    console.log('Name Lower Case: ', toLowerCaseName);
-
-// Validate the input using some conditional logic
+//-------------------------
+userName = prompt('What is your name?');
+let toLowerCaseName = userName.toLowerCase();
+console.log('User name is: ', userName);
+console.log('User name data type is: ', typeof(userName));
+console.log('Name Lower Case: ', toLowerCaseName);
 
 if(toLowerCaseName === 'beth' || toLowerCaseName === 'eva' || toLowerCaseName === 'leah' || toLowerCaseName === 'craig' || toLowerCaseName === 'katie' || toLowerCaseName === 'ruthann'){
-    // we have access to our global variables. 
+  alert('Welcome back to the site, ' + userName);
+} else {
+  alert('Welcome, new friend!');
+}
+//-------------------------
 
-      alert('Welcome back to the site, ' + userName);
-    
-    } else {
-      alert('Welcome, new friend!');
-    }
- // prompt 2
-
- // Prompt users for input
-
- let petInfo= prompt('Do you have a pet?');
- let toUpperCasePetInfo = petInfo.toUpperCase();
- console.log('pet, y or n: ', petInfo);
- console.log('pet info upper case: ', toUpperCasePetInfo);
-
-// Validate the input using some conditional logic
+petInfo = prompt('Do you have a pet?');
+let toUpperCasePetInfo = petInfo.toUpperCase();
+console.log('pet, y or n: ', petInfo);
+console.log('pet info upper case: ', toUpperCasePetInfo);
 
 if(toUpperCasePetInfo === 'Y' || toUpperCasePetInfo === 'YES'){
- // we have access to our global variables. 
+  alert('Congratulations on beng a pet owner, ' + userName + '! I have a dog and two cats.');
+} else {
+  alert('Life is lonely without a pet!');
+}
+//-------------------------
 
- alert('Congratulations on beng a pet owner, ' + userName + '! I have a dog and two cats.');
- 
- } else {
-   alert('Life is lonely without a pet!');
- }
-
-// prompt 3
-
-// Prompt users for input
-
-let nativeInfo = prompt('Are you a Native Iowan');
+nativeInfo = prompt('Are you a Native Iowan');
 let toUpperCaseNativeInfo = nativeInfo.toUpperCase();
 console.log('native, y or n: ', nativeInfo);
 console.log('native info upper case: ', toUpperCaseNativeInfo);
 
-// Validate the input using some conditional logic
 
 if(toUpperCaseNativeInfo === 'Y' || toUpperCaseNativeInfo === 'YES'){
-// we have access to our global variables. 
-
   alert('We are both Native Iowans!');
-
 } else {
   alert('Hmmmm, wonder where you are from?');
 }
+//-------------------------
 
-// prompt 4
-
-// Prompt users for input
-
- let kidInfo = prompt('Do you have children?');
- let toUpperCaseKidInfo = kidInfo.toUpperCase();
- console.log('children?: ', kidInfo);
- console.log('kid info upper case: ', toUpperCaseKidInfo);
-
-// Validate the input using some conditional logic
+kidInfo = prompt('Do you have children?');
+let toUpperCaseKidInfo = kidInfo.toUpperCase();
+console.log('children?: ', kidInfo);
+console.log('kid info upper case: ', toUpperCaseKidInfo);
 
 if(toUpperCaseKidInfo === 'N' || toUpperCaseKidInfo === 'NO'){
- // we have access to our global variables. 
+  alert('Kids are not for everyone, ' + userName) + '!';
+} else {
+  alert('I have children, too ' + userName + '. Two girls to be exact!');
+}
+//-------------------------
 
-   alert('Kids are not for everyone, ' + userName) + '!';
- 
- } else {
-   alert('I have children, too ' + userName + '. Two girls to be exact!');
- }
+travelInfo = prompt('Are you a fan of travelling?');
+let toLowerCaseTravelInfo = travelInfo.toLowerCase();
+console.log('travel info value', travelInfo);
+console.log('travel info lower case ', toLowerCaseTravelInfo);
 
-// prompt 5
+if(toLowerCaseTravelInfo === 'no' || toLowerCaseTravelInfo === 'yes'){
+  alert('Let us head out on adventure, ' + userName + '!');
+} else {
+  alert('This alert should never be triggered!');
+}
 
-// Prompt users for input
 
-  let travelInfo = prompt('Are you a fan of travelling?');
-  let toLowerCaseTravelInfo = travelInfo.toLowerCase();
-  console.log('travel info value', travelInfo);
-  console.log('travel info lower case ', toLowerCaseTravelInfo);
+alert('Hello ' + userName + ' your pet\'s ' + petInfo + ' from iowa? ' + nativeInfo + ' kiddos ' + kidInfo + ' lets travel to ' + travelInfo);
 
-// Validate the input using some conditional logic
-
-if(toLowerCaseTravelInfo === 'no' && toLowerCaseTravelInfo === 'yes'){
-  // we have access to our global variables. 
-
-    alert('Let us head out on adventure, ' + userName + '!');
-  
-  } else {
-    alert('This alert should never be triggered!');
-  }
