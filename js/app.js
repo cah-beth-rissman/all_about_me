2eab5ac48a3647c5e9b96f069794f56c8a0018a3
Beth Rissman <beth.rissman@telepham.com>
2022-11-06 22:27:52 -0600

    questions 5-7 added to js.app, added list to index.html

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 
 'use strict';
+
 console.log('The JS file is alive');
+
 //global variables
-let userName; //creating a space in memory to hold some information later.
+let userName;
 let petInfo;
-let nativeInfo; //declaring variables
+let nativeInfo;
 let kidInfo;
 let travelInfo;
+let siblingInfo;
+let userPoints = 0;
+let favNumber;
+let loopcounter = 0;
 
 
 //-------------------------
+
+function getUserName(){
 userName = prompt('What is your name?');
 let toLowerCaseName = userName.toLowerCase();
 console.log('User name is: ', userName);
@@ -17,60 +25,162 @@ console.log('User name data type is: ', typeof(userName));
 console.log('Name Lower Case: ', toLowerCaseName);
 
 if(toLowerCaseName === 'beth' || toLowerCaseName === 'eva' || toLowerCaseName === 'leah' || toLowerCaseName === 'craig' || toLowerCaseName === 'katie' || toLowerCaseName === 'ruthann'){
-  alert('Welcome back to the site, ' + userName);
+  alert('Welcome back to my All About Me site, ' + userName + '! Let\'s play a guessing game. Each questions you get correct, earns you a point! Please respond with Y or N.');
 } else {
-  alert('Welcome, new friend!');
+  alert('Welcome, ' + userName + '! Let\'s play a guessing game. Each question you get correct, earns you a point! Please respond with Y or N.');
 }
+}
+getUserName();
+
 //-------------------------
 
-petInfo = prompt('Do you have a pet?');
+function getPetInfo(){
+petInfo = prompt('Do I have at least one pet?');
 let toUpperCasePetInfo = petInfo.toUpperCase();
 console.log('pet, y or n: ', petInfo);
 console.log('pet info upper case: ', toUpperCasePetInfo);
 
 if(toUpperCasePetInfo === 'Y' || toUpperCasePetInfo === 'YES'){
-  alert('Congratulations on beng a pet owner, ' + userName + '! I have a dog and two cats.');
+  alert('Correct, I am a pet owner. I have a dog and two cats.');
+  userPoints++;
+  console.log('Logging point scored: ', userPoints);
 } else {
-  alert('Life is lonely without a pet!');
+  alert('Sorry, your response is not correct. I have a dog and two cats.');
 }
+}
+getPetInfo();
+
 //-------------------------
 
-nativeInfo = prompt('Are you a Native Iowan');
+function getNativeInfo(){
+nativeInfo = prompt('Am I a Native Iowan?');
 let toUpperCaseNativeInfo = nativeInfo.toUpperCase();
 console.log('native, y or n: ', nativeInfo);
 console.log('native info upper case: ', toUpperCaseNativeInfo);
 
 
 if(toUpperCaseNativeInfo === 'Y' || toUpperCaseNativeInfo === 'YES'){
-  alert('We are both Native Iowans!');
+  alert('Correct, I am a native Iowan.');
+  userPoints++;
+  console.log('Logging point scored: ', userPoints);
 } else {
-  alert('Hmmmm, wonder where you are from?');
+  alert('That is incorrect, I am a native Iowan.');
 }
+}
+getNativeInfo();
+
 //-------------------------
 
-kidInfo = prompt('Do you have children?');
+function getKidInfo(){
+kidInfo = prompt('Do I have children?');
 let toUpperCaseKidInfo = kidInfo.toUpperCase();
 console.log('children?: ', kidInfo);
 console.log('kid info upper case: ', toUpperCaseKidInfo);
 
 if(toUpperCaseKidInfo === 'N' || toUpperCaseKidInfo === 'NO'){
-  alert('Kids are not for everyone, ' + userName) + '!';
+  alert('Your answer is incorrect, I have two daughters');
 } else {
-  alert('I have children, too ' + userName + '. Two girls to be exact!');
+  alert('Way to go! I have two daughters.');
+  userPoints++;
+  console.log('Logging point scored: ', userPoints);
 }
-//-------------------------
+}
+getKidInfo();
 
-travelInfo = prompt('Are you a fan of travelling?');
+//-------------------------
+function getTravelInfo(){ 
+travelInfo = prompt('Do I enjoy travelling?');
 let toLowerCaseTravelInfo = travelInfo.toLowerCase();
 console.log('travel info value', travelInfo);
 console.log('travel info lower case ', toLowerCaseTravelInfo);
 
-if(toLowerCaseTravelInfo === 'no' || toLowerCaseTravelInfo === 'yes'){
-  alert('Let us head out on adventure, ' + userName + '!');
+if(toLowerCaseTravelInfo === 'n' || toLowerCaseTravelInfo === 'no'){
+  alert('That is incorrect, ' + userName + ' I am always looking for my next adventure!');
 } else {
-  alert('This alert should never be triggered!');
+  alert('Correct, I love to travel!');
+  userPoints++;
+  console.log('Logging point scored: ', userPoints);
 }
+}
+getTravelInfo();
+
+//---------------------------
+function getSiblingInfo(){
+siblingInfo = prompt('Am I the youngest of my siblings?');
+let toLowerCaseSiblingInfo = siblingInfo.toLowerCase();
+console.log('sibling info value', siblingInfo);
+console.log('sibling info lower case ', toLowerCaseSiblingInfo);
+
+if(toLowerCaseSiblingInfo === 'n' || toLowerCaseSiblingInfo === 'no'){
+  alert('That is correct, I am the middle child.');
+  userPoints++;
+  console.log('Logging point scored: ', userPoints);
+} else {
+  alert('Incorrect, I am the middle child.');
+}
+}
+getSiblingInfo();
+
+//-------------------------------number guessing game
+function getFavNumber() {
+
+  while(favNumber !== '16' && loopcounter<5){
+    loopcounter ++;
+    favNumber = prompt('What\'s my favorite number? Guess a number between 1-20 in five or fewer attempts.');
+    if (favNumber >'16'){
+      alert('Your guess is too high! Try again.');
+      console.log(attempts);
+    } else if (favNumber <'16'){
+      alert ('Your guess is too low. Try again');
+      console.log(attempts);
+    } else
+    { alert('Great guess, ' + userName + '. My favorite number is 16!');
+      userPoints++;
+      console.log(attempts);
+    }
+    if (loopcounter === 5)
+    {alert('You\'ve run out of guesses. The correct answer is 16.');
+    } 
+  console.log(favNumber);
+  console.log('Logging point, favNumber: ', userPoints);
+  }
+  }
+  getFavNumber();
+
+  //-----------------------add array
+
+  function getCookieInfo() {
+    let cookieInfo = ['chocolate chip', 'sugar', 'monster', 'snickerdoodle', 'shortbread'];
+    let attempts = 0;
+    let correctAnswer = false;
+    
+    while (attempts !==6 && correctAnswer === false){
+      let cookieInfo = prompt('Cookies are a sweet treat. Guess one of my favorite cookie flavors.');
+      // let cookieInfoToLowerCase = cookieInfoToLowerCase.toLowerCase();
+      if (cookieInfo.indexOf(cookieInfo) >= 0) {
+        userPoints++;
+        console.log('user entered cookie flavor ', cookieInfo);
+        correctAnswer=true;
+        alert('Correct, a ' + cookieInfo + ' cookie is one of my favorite cookies to eat. Chocolate chip, sugar, monster, snickerdoodle and shortbread cookies are all great flavors.');
+      } else {
+        alert('That is not one of my favorites.');
+      }
+      attempts++;
+      console.log('Logging cookie answer', cookieInfo);
+      console.log('Logging cookie answer', userPoints);
+  }
+}
+  getCookieInfo();
+
+  //----------------------add alert
+
+  alert('Hello ' + userName + ', you learned a few things about me and earned some points! You earned ' + userPoints + ' , great job!');
+
+//  add attempt counter, why is everything logging at the end?
+
+// --------------------------------------end of code
 
 
-alert('Hello ' + userName + ' your pet\'s ' + petInfo + ' from iowa? ' + nativeInfo + ' kiddos ' + kidInfo + ' lets travel to ' + travelInfo);
+
+
 
